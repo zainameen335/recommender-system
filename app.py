a8d5930d15bc699df9cc6a4da3380ae0a629dc34
husnain-ameen-nuh-pk <husnain.ameen@nuhtech.pk>
2026-04-04 15:42:58 +0500

    Initial commit of recommender system with similarity matrix

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8e1a18c48281ca6ddb25f9620a53c553&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+movie_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movie_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distance = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x:x[1])[1:6]
+
+    recommended_movie = []
+    movie_poster = []
+    for i in movie_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie.append(movies.iloc[i[0]].title)
+        movie_poster.append(fetch_poster(movie_id))
+
+    return recommended_movie, movie_poster 
+
+
+st.title("Movie Recommender System")
+
+selected_movie_name = st.selectbox(
+    "how whould you like to connected?",
+    movies['title'].values
+)
+
+if st.button('Recommend'):
+    name , poster = recommend(selected_movie_name)
+
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(name[0])
+        st.image(poster[0])
+    with col2:
+        st.text(name[1])
+        st.image(poster[1])
+    with col3:
+        st.text(name[2])
+        st.image(poster[2])
+    with col4:
+        st.text(name[3])
+        st.image(poster[3])
+    with col5:
+        st.text(name[4])
+        st.image(poster[4])        
